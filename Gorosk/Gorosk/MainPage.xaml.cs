231b246191883fb5f9757253fb4763cccbe2ef46
svea <vihterlana@mail.ru>
2022-04-21 23:13:54 +0300

    dob

--- Gorosk/Gorosk/MainPage.xaml.cs
+++ Gorosk/Gorosk/MainPage.xaml.cs
@@ -10,11 +10,40 @@ namespace Gorosk
 {
     public partial class MainPage : ContentPage
     {
-       // List<horosk> horosks;
-       // string[] horoskTit;
+        Button Gor_btn, Aja_btn;
+        // List<horosk> horosks;
+        // string[] horoskTit;
         public MainPage()
         {
-           // horosks = horosk.Gethorosks();
+            Button Gor_btn = new Button
+            {
+                Text = "ГОРОСКОП",
+                BackgroundColor = Color.Plum
+            };
+            Button Aja_btn = new Button
+            {
+                Text = "РАСПИСАНИЕ",
+                BackgroundColor = Color.Coral
+            };
+            // horosks = horosk.Gethorosks();
+            StackLayout st = new StackLayout
+            {
+                Children = { Gor_btn, Aja_btn, }
+            };
+            st.BackgroundColor = Color.Aqua;
+            Content = st;
+            Gor_btn.Clicked += Gor_btn_Clicked;
+            Aja_btn.Clicked += Aja_btn_Clicked;
+        }
+
+        private async void Aja_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ajaplaan());
+        }
+
+        private async void Gor_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GOROS());
         }
     }
 }
